049f3217b0a853f0e0ad8d33b2a0b60291c5ea43
Dhananjay Pawar <dhananjay1796@gmail.com>
2022-06-09 13:08:23 +0530

    Features  Added: Added Factorial code                         Adde sum code

--- maths/factorial.c
+++ maths/factorial.c
@@ -1,8 +1,12 @@
-#include<stdio.h>
+i#include<stdio.h>
 
 int factorial(int n);
 
-int main(){
+int factorial(int n){
+ if (n==1 || n==0)
+	 return 1;
+ else 
+     return n* factorial(n-1);
 
 }
 
--- maths/sum.c
+++ maths/sum.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
 
-int sum(int n,int m){}
+int sum(int n,int m);
+
+int sum(int n, int m){
+
+	return (n+m);
+}
+
+int main(){
+}
+
 
