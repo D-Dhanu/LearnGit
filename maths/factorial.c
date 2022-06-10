ab9c989660570d6af86f59ae2fbeb9c9ada8ecb5
D-Dhanu <107106999+D-Dhanu@users.noreply.github.com>
2022-06-10 11:29:30 +0530

    Update factorial.c

--- maths/factorial.c
+++ maths/factorial.c
@@ -3,10 +3,9 @@ i#include<stdio.h>
 int factorial(int n);
 
 int factorial(int n){
- if (n==1 || n==0)
-	 return 1;
+ if (n==1 )
+	return 1;
  else 
-     return n* factorial(n-1);
-
+    return 0;
 }
 
